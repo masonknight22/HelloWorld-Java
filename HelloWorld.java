01c646bfcb06e853b3f590de61e3e86f55619a02
Mason Knight <92546628+masonknight22@users.noreply.github.com>
2022-05-02 23:49:52 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+/**
+ * HelloWorld
+ *
+ * A program that states "Hello World!"
+ *
+ * Purdue University - CS180
+ *
+ * Author: Mason Knight
+ *
+ * Version: January 29 2021
+ *
+ *
+ */
+
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+
+
+}
